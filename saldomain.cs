6165e0e88984b28f597ebd4bde9b86152dfa2dce
Gabs <cardosogabrielle601@gmail.com>
2025-09-15 22:44:42 -0300

    Add files via upload

---
+++ saldomain.cs
@@ -0,0 +1,11 @@
+﻿int saldo = 0;
+if(saldo > 0){
+    Console.WriteLine("Saldo positivo");
+} else if(saldo < 0){
+    Console.WriteLine("Saldo negativo");
+} else {
+    Console.WriteLine("Saldo zerado");
+}
+
+
+// testes de códigos utilizando if/ else if /else 
